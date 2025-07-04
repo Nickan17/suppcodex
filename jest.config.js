fbe1e030af49dcf98e6de3d8c11f866d94f70a58
Roo <roo@suppcodex.com>
2025-07-03 19:25:41 -0700

    test: rely on pure jest-expo preset, remove custom transforms

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,8 @@
 /* eslint-env node */
 module.exports = {
-  preset: 'jest-expo',                  // ← published preset for Expo 53
+  preset: 'jest-expo',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-runner|@react-native|react-native'
-    + '|@react-native/community|expo(nent)?|@expo(nent)?|expo-modules-core|expo-status-bar|expo-constants'
-    + '|react-navigation|@react-navigation'
-    + '|@unimodules|unimodules|sentry-expo|native-base|@sentry)/)',
-  ],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/$1',
     '^@/(.*)$': '<rootDir>/$1',
