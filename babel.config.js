c11c112e518e2d8c3e90dc76cf2db7748b826840
Roo <roo@suppcodex.com>
2025-07-07 07:29:24 -0700

    Update configuration files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+  ],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ],
+    env: {
+      test: {
+        plugins: [
+          '@babel/plugin-transform-runtime',
+          'dynamic-import-node',
+        ],
+      },
+    },
   };
 };
--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,15 @@ module.exports = {
     '\\.(png|jpe?g|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
     '^expo/src/.*$': '<rootDir>/__mocks__/expoWinterStub.js',
   },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native(-.*)?|@expo(-.*)?/.*|@expo/vector-icons/.*|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|expo-modules-core|@supabase/.*|isows/.*|react-native-url-polyfill|whatwg-fetch)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    __DEV__: true,
+  },
+  testMatch: ['**/__tests__/**/*.test.(js|jsx|ts|tsx)'],
 };
